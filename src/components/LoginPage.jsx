9bebd5035c6c5348a4ac9d9155d27537098c8778
Hưng Nguyễn <hungnguyen@Hungs-MacBook-Air.local>
2023-11-11 16:48:56 +0700

    add file modifile

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,7 +4,7 @@ import Login from "./Login";
 function LoginPage({ onLogin }) {
   return (
     <div className="loginPage">
-      <h1>Welcome to Shop Cart !</h1>
+      <h1 className="loginPage-title">Welcome to Shop Cart !</h1>
       <Login onLogin={onLogin} />
     </div>
   );
